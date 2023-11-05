41a934ad7c1a3b67e863273bd5776e716899931a
digital evolutions <digitalevolutions@digitals-MacBook-Air.local>
2023-11-05 10:11:47 +0530

    Profile update 001

--- pages/profile.js
+++ pages/profile.js
@@ -14,17 +14,17 @@ export async function getServerSideProps(context){
   const session = await getSession({ req: context.req });
   console.log(session);
 
-  /*if(!session){
+ /* if(!session){
     return {
       redirect : {
         destination : '/auth',
         permanent : false
       }
     }
-  }
+  }*/
 
   return {
     props: {session},
-  }*/
+  }
 
 }
